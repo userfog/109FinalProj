3093c12dd8de64ad05d77287313903ae630dfad9
Zachary Fogelson <fogelsonz606@gmail.com>
2014-12-02 19:13:40 -0500

    incase you need to regen the players csv

---
+++ num_players_to_df/quick_create_full_df.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import StringIO as sio
+import io
+import csv
+import pprint
+import datetime
+import collections
+import copy
+pp = pprint.PrettyPrinter(indent=8)
+
+player_columns = ["player", "timestamp", "play", "pos", "prflop", "flop", "turn", "river", "bankroll", "action", "winnings", "c1", "c2"]
+
+def main():
+	full = pd.DataFrame(columns=player_columns)
+	for i in xrange(2, 11):
+	    print "Num Players = " + str(i)
+	    helper = pd.read_csv(("num_players_%d.csv" % i)) 
+	    full = full.append(helper)
+	full.to_csv("all_players.csv")
+
+if __name__ == '__main__':
+	main()
